a4c92667da6bc125bb4c69145134bd897edc4dee
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-11-16 11:01:16 +0700

    add feature start_date, end_date, and handle request limit

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ KEYWORD = '"fun fact" lang:en'
 TARGET_RESULTS = 100000
 SAVING_PATH = 'fun_fact_004.csv'
 END_TIME = None # in the future, will be fetched first
-START_TIME = '2021-11-16T01:45:57.000Z' # in the pas, will be fetched at the end
+START_TIME = '2021-11-16T01:45:57.000Z' # in the past, will be fetched at the end
+
 search(
     keyword=KEYWORD,
     maximum_result=TARGET_RESULTS,
