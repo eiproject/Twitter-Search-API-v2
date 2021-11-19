3d9942b6e74f43194b1a4fda76c85ffbad11ea55
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-11-19 11:22:41 +0700

    new

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from core.search import search
 
 KEYWORD = '"fun fact" lang:en'
 TARGET_RESULTS = 100000
-SAVING_PATH = 'fun_fact_006.csv'
+SAVING_PATH = 'fun_fact_007.csv'
 END_TIME = None # in the future, will be fetched first
-START_TIME = '2021-11-17T01:58:52.000Z' # in the past, will be fetched at the end
+START_TIME = '2021-11-18T00:09:31.000Z' # in the past, will be fetched at the end
 
 search(
     keyword=KEYWORD,
--- run_terrorist.py
+++ run_terrorist.py
@@ -2,9 +2,9 @@ from core.search import search
 
 KEYWORD = 'teroris OR terroris lang:id'
 TARGET_RESULTS = 100000
-SAVING_PATH = 'terroris_002.csv'
+SAVING_PATH = 'terroris_003.csv'
 END_TIME = None # in the future, will be fetched first
-START_TIME = '2021-11-17T03:03:02.000Z' # in the past, will be fetched at the end
+START_TIME = '2021-11-19T03:19:31.000Z' # in the past, will be fetched at the end
 
 search(
     keyword=KEYWORD,
