7eeb6df695b6bb2ad42f04b6e7ce63fca9d12219
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2022-01-30 22:21:04 +0700

    skip append and split when the big file path is not exists

--- run_terrorist.py
+++ run_terrorist.py
@@ -23,10 +23,11 @@ saving_paths = SAVING_PATH.split('.')
 new_saving_path = saving_paths[0] + ' +' + str(search_result - 1) + '.' + saving_paths[-1]
 os.rename(SAVING_PATH, new_saving_path)
 
-append_csv(BIG_FILE_PATH, new_saving_path)
+if (os.path.isdir(BIG_FILE_PATH)):
+    append_csv(BIG_FILE_PATH, new_saving_path)
 
-split_csv_to(
-    source_csv=BIG_FILE_PATH,
-    num=100000,
-    result_csv=BIG_FILE_PATH
-)
+    split_csv_to(
+        source_csv=BIG_FILE_PATH,
+        num=100000,
+        result_csv=BIG_FILE_PATH
+    )
